9979d0715ba19fc54c0259807175ebe0cb4541d5
Ezra003 <126328199+Ezra003@users.noreply.github.com>
2025-04-18 20:59:54 +0200

    twitter

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { ReactNode } from 'react'
 import { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog, Authors } from 'contentlayer/generated'
@@ -8,6 +9,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import { TwitterTweetEmbed } from 'react-twitter-embed'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
@@ -95,9 +97,12 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
               <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(path)} rel="nofollow">
-                  Discuss on Twitter
-                </Link>
+                {/* Twitter Embed replaces Discuss on Twitter */}
+                {content.tweetId ? (
+                  <div className="flex justify-center">
+                    <TwitterTweetEmbed tweetId={content.tweetId} />
+                  </div>
+                ) : null}
               </div>
               <div
                 className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300"
